a68bd816792ba8216fba4c5f3ea3742244748823
juliapresa <juliapresabosco@gmail.com>
2023-11-06 15:08:07 -0300

    primer push

---
+++ carolina/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <p>
+            Tiempo de mujeres <br />
+            Tiempo de hacer múltiples cosas a la vez y que todas salgan bien.<br />
+            Tiempo de imaginar un mundo distinto y planificar como llegar a él.<br />
+            Tiempo de aprovechar nuestro conocimiento científico que es científica.<br />
+            Tiempo de incidir a nivel regional e internacional con iniciativa y decisión.<br />
+            Tiempo de gestionar con una larga experiencia acumulada teniendo claro el rumpo.<br />
+            Tiempo de mirar cerca con destino lejano.<br />
+            Tiempo de estar a la altura de los problemas de la gente y plantear soluciones reales.<br />
+            Tiempo de estudiar los temas y tomar la mejor decisión.<br />
+            Tiempo de cambios profundos comon toda primera vez.<br />
+            Tiempo de dejar de doblar listas para colocar la banda.<br />
+            Tiempo de dar un paso al Frente.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
